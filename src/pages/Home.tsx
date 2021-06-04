f762efd5e82c1c6b45a23c988b816b4bd3098eb2
Tyler S-C <tscizzle@gmail.com>
2021-06-04 16:08:10 -0400

    Tons of progress on login page, trying out ionic components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useReducer } from "react";
 import {
-  IonContent,
-  IonHeader,
   IonPage,
-  IonTitle,
+  IonHeader,
+  IonContent,
   IonToolbar,
+  IonTitle,
+  IonButtons,
   IonButton,
+  IonImg,
+  IonItem,
+  IonLabel,
   IonInput,
+  IonCard,
+  IonCardContent,
 } from "@ionic/react";
+import _ from "lodash";
 
-import { User } from "models";
 import api from "api";
+import { User } from "models";
+
+import logo from "assets/images/calendar.svg";
 
 import "./Home.css";
 
-class Home extends React.Component {
+type HomeProps = {};
+
+type HomeState = {
+  email: string;
+  password: string;
+  loginErr: string;
+  registerErr: string;
+  loggedInUser: User | null;
+};
+
+class Home extends React.Component<HomeProps, HomeState> {
   state = {
     email: "",
     password: "",
@@ -30,39 +49,59 @@ class Home extends React.Component {
   }
 
   render() {
+    const loggedInUser = this.state.loggedInUser;
+    let username = _.isNull(loggedInUser)
+      ? ""
+      : (loggedInUser as User).username;
+    const shortUsername = _.first(_.split(username, "."));
     return (
       <IonPage>
         <IonHeader>
-          <IonToolbar>
-            <IonTitle>Blank</IonTitle>
+          <IonToolbar color="primary">
+            <IonTitle>Nice Calendar</IonTitle>
+            <IonButtons slot="secondary">
+              <IonButton>{username.split(".")[0]}</IonButton>
+            </IonButtons>
           </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Blank</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonInput
-            name="email"
-            type="text"
-            value={this.state.email}
-            onIonChange={(evt) => {
-              const inputEle = evt.target as HTMLInputElement;
-              this.setState({ email: inputEle.value });
-            }}
-          />
-          <IonInput
-            name="password"
-            type="password"
-            value={this.state.password}
-            onIonChange={(evt) => {
-              const inputEle = evt.target as HTMLInputElement;
-              this.setState({ password: inputEle.value });
-            }}
-          />
-          <IonButton onClick={this.attemptLogin}>Login</IonButton>
-          {JSON.stringify(this.state.loggedInUser)}
+        <IonContent>
+          <IonImg className="cal-logo" src={logo} alt="" />
+          <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput
+              name="email"
+              type="text"
+              value={this.state.email}
+              onIonChange={(evt) => {
+                const inputEle = evt.target as HTMLInputElement;
+                this.setState({ email: inputEle.value });
+              }}
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Password</IonLabel>
+            <IonInput
+              name="password"
+              type="password"
+              value={this.state.password}
+              onIonChange={(evt) => {
+                const inputEle = evt.target as HTMLInputElement;
+                this.setState({ password: inputEle.value });
+              }}
+            />
+          </IonItem>
+          <IonButton
+            className="ion-margin"
+            expand="block"
+            onClick={this.attemptLogin}
+          >
+            Login
+          </IonButton>
+          {username && (
+            <IonCard>
+              <IonCardContent>{username}</IonCardContent>
+            </IonCard>
+          )}
         </IonContent>
       </IonPage>
     );
